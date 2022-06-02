a8f122996bafc5c973bd71ecf681616f4fee8ed7
Andrii Rastorhuiev <efinswim@gmail.com>
2022-06-02 18:05:17 +0300

    added users board, hook, connect store

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
+import useStore from './hooks/useStore';
+
 function App() {
+  const {users} = useStore()
+  console.log(users)
+
   return <div></div>;
 }
 
--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,10 @@
 import qs from 'query-string'
 
-export const DOMAIN = 'http://localhost:3001/'
+export const DOMAIN = 'http://localhost:3001'
 
 class ApiCall {
   constructor(domain) {
-    this.domain = domain
+    this.domain = domain;
   }
 
   async perform(url, data, config) {
@@ -12,33 +12,33 @@ class ApiCall {
       ...config,
       body: JSON.stringify(data),
       headers: {
-        'Content-type': 'application/json'
-      }
-    })
+        'Content-type': 'application/json',
+      },
+    });
 
-    return await request.json()
+    return await request.json();
   }
 
   async get(path, searchParams = {}) {
-    return await this.perform(`${path}?${qs.stringify(searchParams)}`)
+    return await this.perform(`${path}?${qs.stringify(searchParams)}`);
   }
 
   async post(path, payload) {
     return await this.perform(path, payload, {
-      method: 'POST'
-    })
+      method: 'POST',
+    });
   }
 
   async put(path, payload) {
     return await this.perform(path, payload, {
-      method: 'PUT'
-    })
+      method: 'PUT',
+    });
   }
 
   async delete(path) {
     return await this.perform(path, {
-      method: 'DELETE'
-    })
+      method: 'DELETE',
+    });
   }
 }
 
